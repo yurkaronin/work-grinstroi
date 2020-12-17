c2e5a89f82248c7b655a179df475b1152798314c
Юрий Ронин <47382042+yurkaronin@users.noreply.github.com>
2020-12-17 20:52:22 +0300

    добавил фавиконку

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,30 @@
 "use strict";
+
 window.addEventListener("scroll", function(){
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+/* Прогресс бар в шапке сайта  */
+var progress = document.querySelector('.progress-bar');
+
+window.addEventListener('scroll', progressBar);
+
+function progressBar(e) {
+  var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var result = windowScroll / windowHeight * 100;
+
+  progress.style.width = result + '%';
+}
+
+wow = new WOW(
+  {
+    boxClass: 'wow',      // default
+    animateClass: 'animated', // default
+    offset: 0,          // default
+    mobile: true,       // default
+    live: true        // default
+  }
+)
+wow.init();
